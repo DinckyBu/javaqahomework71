c57ae7b674000cdb436962caed0da68a90ed67ad
Ekaterina Kobeleva <kobelevaekaterina09@gmail.com>
2023-05-05 19:14:26 +0300

    Changed names tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,7 +18,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    public void findMax1() {
+    public void findMaxWhenThereIsZero() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 0};
@@ -30,7 +30,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    public void findMax2() {
+    public void findMaxWhenThereIsLargeNumber() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 10_000, 8, 6, 11, 11, 12, 0};
