6619ca193c407177cf0717e20cc34e560d1397a2
Ekaterina Kobeleva <kobelevaekaterina09@gmail.com>
2023-05-05 19:05:23 +0300

    Added new tests

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class StatisticsServiceTest {
+
+    @Test
+    public void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 0};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 10_000, 8, 6, 11, 11, 12, 0};
+        long expected = 10_000;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
